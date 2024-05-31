d664ca7aaa191a98101914d72e38588af66b5fd6
saqib0335 <141274831+saqib0335@users.noreply.github.com>
2024-05-31 13:12:58 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
 import './App.css';
+import Hero from './components/Hero';
+import About from './components/About';
+import Technologies from './components/Technologies';
+import Experience from './components/Experience';
+import Project from './components/Project';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="overflow-x-hidden text-neutral-300 antialiased slection:bg-cyan-300 selection:text-cyan-900">
+      <div className='fixed top-0 -z-10 h-full w-full'>
+      <div class="absolute top-0 z-[-2] h-screen 
+           w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+         </div>
+      </div>
+         <div className='container mx-auto px-8'>
+         <Navbar />
+         <Hero />
+         <About />
+         <Technologies />
+         <Experience />
+         <Project/>
+         <Contact/>
+         </div>
     </div>
+  
   );
 }
 
